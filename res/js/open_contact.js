34422498ec76cb2716ac2f6b547bc540a0d173e6
arushsharma <arush24astar@gmail.com>
2020-10-08 15:57:40 +0530

    update contact page

---
+++ res/js/open_contact.js
@@ -0,0 +1,13 @@
+function openContact(idx) {
+    switch(idx) {
+        case 0:
+            window.open('https://github.com/arushsharma24');
+            break;
+        case 1:
+            window.open('https://www.facebook.com/profile.php?id=100015352274068');
+            break;
+        case 2:
+            window.open('https://www.instagram.com/aaaruushhh/');
+            break;
+    }
+}
