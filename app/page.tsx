bb3beb58598b0cc731771830abb2622bc9183a73
Arush Sharma <arush24astar@gmail.com>
2024-02-23 03:03:40 +0530

    add data and check commitsign

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my portfolio</h1>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">hi! &nbsp; i'm arush :) </h1>
       <p className="prose prose-neutral dark:prose-invert">
-        This is your new portfolio.
+        This is my website.
+        <br />
+        There's not much stuff on here yet, but I will be adding more here soon. 
+        <br />
+        Till then, you can check out my <a href="https://linkedin.com/in/arushsharma24">LinkedIn</a> or <a href="https://github.com/arushsharma24">GitHub</a> profiles here!
+        <br />
+        <br />
+        Cheers!
       </p>
     </section>
   );
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -4,10 +4,11 @@ export default function Page() {
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">my work</h1>
       <p className="prose prose-neutral dark:prose-invert">
         I'm a Software Engineer @ Cisco.
-        <br></br>
+        <br/>
         B.Tech IIT Roorkee ECE '23 Grad
-        <br></br>
-        
+        <br/>
+        <br/>
+        You can checkout my resume at this link: <a href="https://drive.google.com/drive/folders/1Uk8DVEwPDeghiuOB2uUn_KridDPnLQOc?usp=sharing"> Arush Sharma CV </a>
         </p>
     </section>
   );
