62274f9176a0f83eb29674b5e8d69b50eed1d755
Arush Sharma <arush24astar@gmail.com>
2024-02-25 02:07:15 +0530

    fix basic blog functionality

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,60 @@
-export default function Page() {
-    return (
-      <section>
-        <h1 className="font-medium text-2xl mb-8 tracking-tighter">my blog</h1>
-        <p className="prose prose-neutral dark:prose-invert">
-          This site is a work in progress. Will be adding my blogs here soon!
-        </p>
-      </section>
-    );
-  }
-  
+import Link from 'next/link';
+import { Suspense } from 'react';
+// import ViewCounter from './view-counter';
+// import { getViewsCount } from 'app/db/queries';
+import { getBlogPosts } from '../db/blog';
+
+export const metadata = {
+  title: 'Blog',
+  description: 'Read my thoughts on software development, design, and more.',
+};
+
+export default function BlogPage() {
+  let allBlogs = getBlogPosts();
+
+  return (
+    <section>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">
+        my blog posts
+      </h1>
+      {/* <p>
+      I write blogs on just about anything that interests me at that moment.
+      </p> */
+      // !TODO: Add a descripttion section/one liner for the blog page.
+      // !TODO: Add a search bar for the blog page.
+      // !TODO: Add calendar features with the blog / dates related stuff
+      }
+      {allBlogs
+        .sort((a, b) => {
+          if (
+            new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
+          ) {
+            return -1;
+          }
+          return 1;
+        })
+        .map((post) => (
+          <Link
+            key={post.slug}
+            className="flex flex-col space-y-1 mb-4"
+            href={`/blog/${post.slug}`}
+          >
+            <div className="w-full flex flex-col">
+              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+                {post.metadata.title}
+              </p>
+              {/* <Suspense fallback={<p className="h-6" />}>
+                <Views slug={post.slug} />
+              </Suspense> */}
+            </div>
+          </Link>
+        ))}
+    </section>
+  );
+}
+
+// async function Views({ slug }: { slug: string }) {
+//   let views = await getViewsCount();
+
+//   return <ViewCounter allViews={views} slug={slug} />;
+// }
