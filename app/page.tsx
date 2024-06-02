ce6b6746dabcc55fcceb26b5e3897edbe9f3b426
Arush Sharma <arush24astar@gmail.com>
2024-06-03 00:32:45 +0530

    update page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
     <section>
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">hi! &nbsp; i&apos;m arush :) </h1>
       <p className="prose prose-neutral dark:prose-invert">
-        This is my website.
+        I&apos;m currently a software engineer at <Link href="https://www.empirical.run/">Empirical</Link>, before which I worked at <Link href="https://www.cisco.com/">Cisco</Link>
         <br />
-        There&apos;s not much stuff on here yet, but I will be adding more here soon. 
         <br />
-        Till then, you can check out my <a href="https://linkedin.com/in/arushsharma24">LinkedIn</a> or <a href="https://github.com/arushsharma24">GitHub</a> profiles here!
+        I (wish) &amp; love to travel and explore the world, think and exchange ideas with other people and enjoy life.
         <br />
         <br />
         Cheers!
+        <br /><br />
+        <a href="https://linkedin.com/in/arushsharma24">LinkedIn</a>
+        <br />
+        <a href="https://github.com/arushsharma24">GitHub</a> 
+        <br />
       </p>
     </section>
   );
