969ec411d829937008b6f711a1f7ae81af927bb3
Arush Sharma <arush24astar@gmail.com>
2024-09-15 18:37:58 +0530

    fix: update to latest build

---
+++ out/_next/static/chunks/app/page-1ce7448077ddcd26.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,404],{8174:function(n,e,u){Promise.resolve().then(u.t.bind(u,4695,23))}},function(n){n.O(0,[695,834,459,744],function(){return n(n.s=8174)}),_N_E=n.O()}]);
